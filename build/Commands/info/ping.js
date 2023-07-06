b90427e8b9a73179f8a2277fbe135686a89feb8b
QuinnVN <96212613+QuinnVN@users.noreply.github.com>
2023-07-06 11:27:57 +0700

    Delete build directory

--- build/Commands/info/ping.js
+++
@@ -1,9 +0,0 @@
-import { SlashCommandBuilder } from "discord.js";
-import Command from "../../module/types/Command.js";
-async function pingCommand(interaction) {
-    interaction.reply(`Bot's ping: ${interaction.client.ws.ping}`);
-}
-export default new Command({
-    data: new SlashCommandBuilder().setName("ping").setDescription("Show bot's ping"),
-    run: pingCommand,
-});
