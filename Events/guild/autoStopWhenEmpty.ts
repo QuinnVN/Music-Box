90aff7847471b8bb803db1fda3bb394afeac0d17
QuinnVN <96212613+QuinnVN@users.noreply.github.com.>
2024-06-09 22:27:25 +0700

    fix: wrong order when stopping looped songs and queues

--- Events/guild/autoStopWhenEmpty.ts
+++ Events/guild/autoStopWhenEmpty.ts
@@ -4,8 +4,8 @@ import MusicBoxClient from "../../MusicBox.js";
 import Event from "../../module/structures/Event.js";
 
 function fullStop(player: Player): void {
-  player.setQueueRepeat(false);
   player.setTrackRepeat(false);
+  player.setQueueRepeat(false);
   player.queue.clear();
   player.stop();
 }
